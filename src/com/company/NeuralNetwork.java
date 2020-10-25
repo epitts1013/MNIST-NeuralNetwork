c86bf104cd43a1689d3983974e906b1146ce09f7
Eric Pitts <epitts1013@gmail.com>
2020-10-25 01:50:17 -0500

    Update NeuralNetwork, Update Neuron

--- src/com/company/NeuralNetwork.java
+++ src/com/company/NeuralNetwork.java
@@ -2,5 +2,41 @@ package com.company;
 
 public class NeuralNetwork
 {
+    // neuron arrays
+    private Neuron[] inputLayer;
+    private Neuron[][] midLayers;
+    private Neuron[] outputLayer;
 
+    public NeuralNetwork(int numInputs, int numMidLayers, int numMidLayerNodes, int numOutputLayerNodes)
+    {
+        // initialize neuron arrays
+        inputLayer = new Neuron[numInputs];
+        midLayers = new Neuron[numMidLayers][numMidLayerNodes];
+        outputLayer = new Neuron[numOutputLayerNodes];
+
+        // initialize neurons in input layer
+        for (int i = 0; i < numInputs; i++)
+            inputLayer[i] = new Neuron();
+
+        // initialize neurons in first mid layer
+        for (int i = 0; i < numMidLayerNodes; i++)
+            midLayers[0][i] = new Neuron(inputLayer);
+
+        // initialize neurons in all other mid layers
+        for (int i = 1; i < numMidLayers; i++)
+        {
+            for (int j = 0; j < numMidLayerNodes; j++)
+                midLayers[i][j] = new Neuron(midLayers[i-1]);
+        }
+
+        // initialize neurons in final layer
+        for (int i = 0; i < numOutputLayerNodes; i++)
+            outputLayer[i] = new Neuron(midLayers[numMidLayers-1]);
+    }
+
+    public void InputNetworkData(NetworkInput input)
+    {
+        for (int i = 0; i < inputLayer.length; i++)
+            inputLayer[i].SetOutput(input.inputValues[i]);
+    }
 }
--- src/com/company/Neuron.java
+++ src/com/company/Neuron.java
@@ -3,27 +3,32 @@ import java.lang.Math;
 
 public class Neuron
 {
+    // sets whether neuron is an input neuron, if neuron is an input neuron its output can be set manually
+    final boolean INPUT_NEURON;
+
     // class variables
     private double bias, output;
     private double[] weights;
     private final Neuron[] inputs;
 
-    // constructor for neuron with a double array as inputs
-//    public Neuron(int numInputs)
-//    {
-//        // initialize variables
-//        bias = Math.random();
-//        inputs = null;
-//        weights = new double[numInputs];
-//
-//        // generate random initial weights
-//        for (int i = 0; i < weights.length; i++)
-//            weights[i] = Math.random();
-//    }
+    // Constructor for neuron used for network input
+    public Neuron()
+    {
+        // set neuron as input neuron
+        INPUT_NEURON = true;
+
+        // initialize variable as default values
+        bias = 0;
+        inputs = null;
+        weights = null;
+    }
 
     // Constructor for neuron with neuron outputs as its inputs
     public Neuron(Neuron[] inputs)
     {
+        // set neuron as not input neuron
+        INPUT_NEURON = false;
+
         // initialize variables
         bias = Math.random();
         this.inputs = inputs;
@@ -34,55 +39,28 @@ public class Neuron
             weights[i] = Math.random();
     }
 
-    // version of CalculateOutput for use with neurons that recieve input from other neurons (i.e. all layers besides first layer)
+    // Calculates output of neuron
+    // If neuron is initialized as an input neuron, just returns output
+    // Yes, there's already a GetOutput(), I need it for other methods, I don't want errors if
+    // somebody tries to use this method on an input neuron
     public double CalculateOutput()
     {
-        // zero out output
-        double sum = 0;
-
-        try
+        // if not in input neuron, proceed as normal
+        if (!INPUT_NEURON)
         {
+            // zero out output
+            double sum = 0;
+
             // perform dot-product
             for (int i = 0; i < weights.length; i++)
                 sum += inputs[i].GetOutput() * weights[i];
+
+            // add bias to sum
+            sum += bias;
+
+            // use sum as input for sigmoid function
+            output = 1 / (1 + Math.pow(Math.E, -sum));
         }
-        catch (ArrayIndexOutOfBoundsException e)  // tried to run normal output calculation on neuron initialized for direct input
-        {
-            System.out.println("Neuron was initialized for direct input, please use overloaded method CalculateOutput(double[] directInputs)");
-        }
-
-        // add bias to sum
-        sum += bias;
-
-        // use sum as input for sigmoid function
-        output = 1 / (1 + Math.pow(Math.E, -sum));
-
-        // return output
-        return output;
-    }
-
-    // version of CalculateOutput for use with neurons that recieve direct input (i.e. first layer neurons)
-    public double CalculateOutput(double[] directInputs)
-    {
-        // zero out output
-        double sum = 0;
-
-        try
-        {
-            // perform dot-product
-            for (int i = 0; i < weights.length; i++)
-                sum += directInputs[i] * weights[i];
-        }
-        catch (ArrayIndexOutOfBoundsException e)  // length of directInputs does not match length of weights
-        {
-            System.out.println("Length of double array must match number of weights");
-        }
-
-        // add bias to sum
-        sum += bias;
-
-        // use sum as input for sigmoid function
-        output = 1 / (1 + Math.pow(Math.E, -sum));
 
         // return output
         return output;
@@ -92,5 +70,14 @@ public class Neuron
     public double GetOutput()
     {
         return output;
+    }
+
+    // setter for output, does not work if INPUT_NEURON is not true
+    public void SetOutput(double value)
+    {
+        if (INPUT_NEURON)
+            output = value;
+        else
+            System.out.println("Value for neuron cannot be manually set if it is not initialized as an input neuron");
     }
 }
